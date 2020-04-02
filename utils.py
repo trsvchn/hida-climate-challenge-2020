87ba1abe1294b0060eac08b6e17aedcf19b2c67b
Taras Savchyn <trsvchn@gmail.com>
2020-04-02 16:37:21 +0300

    Add import utils

---
+++ utils.py
@@ -0,0 +1,26 @@
+"""Utils.
+"""
+import xarray as xr
+import pandas as pd
+
+
+def load_forcing_as_df(file_path: str) -> pd.DataFrame:
+    """Impors forcing time series as pandas df.
+    """
+    data = xr.open_dataset(file_path)
+    df = data.to_dataframe()
+    # Multiindex to single index
+    df = df.reset_index(level=['lev', 'time', 'x'])
+    # Ugly hack for a column of time
+    df['time'] = (df['time'] // 10000).astype(int)
+    return df
+
+
+def load_geodata_as_df(file_path: str) -> pd.DataFrame:
+    """Imports geodata as pandas df.
+    """
+    data = xr.open_dataset(file_path)
+    df = data.to_dataframe()
+    df = df.reset_index(level=['lat', 'lon', 'time'])
+    df['time'] = (df['time'] // 10000).astype(int)  # Ugly hack for a column of time
+    return df
